dcdbc19bf683cba8d43ac60cfa93a32658e732f5
koreylgray13 <87346809+koreylgray13@users.noreply.github.com>
2022-06-08 20:47:07 -0400

    Add files via upload

---
+++ PasswordDatabase.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import sqlite3
+
+
+root = Tk()
+root.title('Password Manager')
+root.geometry('400x400')
+
+# Create / Connect Database
+conn = sqlite3.connect('passwords.db')
+
+# Create Cursor
+c = conn.cursor()
+
+# Create Table
+c.execute("""CREATE TABLE passwords (
+        name text, 
+        username text,
+        password text, 
+        category text
+        )""")
+
+# Commit Changes
+conn.commit()
+
+# Close Connection
+conn.close()
+
+# Open GUI
+root.mainloop()
